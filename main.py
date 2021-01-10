2c8706cc2305d690c0d6b0054b40e940a1752028
OleChristianAGuldahl <olechristiangulldal@gmail.com>
2021-01-10 15:45:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,32 @@ poengsum=0
 svar = ""
 
 # Lager en løkke, hvor spilleren kan skrive inn tall
-
-#Hvis svaret er ikke 99 sjekkes svaret mot det tilfeldige tallet. 
+# Hvis svaret er ikke 99 sjekkes svaret mot det tilfeldige tallet
+# Løkken forsetter helt til spilleren skriver inn 99
 while svar != "99":
-    svar = input("Gjett ett tall mellom 0 og 10 (skriv 99 for å avslutte spillet): ")
-    if int(svar) == int(tilfeldig_tall):
-        # legger til ett poeng til poeng summen hvis riktig 
-        poengsum +=1
-        print ("Riktig, din poensum er nå: " + str(poengsum))
-        tilfeldig_tall = random.randint(0, 2)
-        #Hvis spillern gjettet feil
-    else:
-        print("Desverre feil, gjett igjen")
+    # Legger på feilhåndtering, så programmet ikke avsluttes viss spillern skriver en bokstav
+    try:
+        svar = input("Gjett ett tall mellom 0 og 3(skriv 99 for å avslutte spillet): ")
+        
+        if int(svar) == int(tilfeldig_tall):
+            # Merk vi konverterer både svar tilfeldig tall til heltall
+            # Legger til ett poeng til poeng summen hvis riktig 
+            poengsum +=1
+            print ("Riktig, din poensum er nå: " + str(poengsum))
+            # Merk vi konverterer poengsummen til string når den skal skrives ut
+            tilfeldig_tall = random.randint(0, 2)
+
+            # Hvis spiler skriver 99. Skriver hadet bra
+        elif int(svar) == int(99):
+            print("Avslutt")
+
+            # Hvis spillern gjettet feil
+        else:
+            print("Desverre feil, gjett igjen")
+
+    # Ved feil gis tilbakemelding til spillern
+    except ValueError:
+        print("SKRIV TALL DIN TAPER!!")
+        
+    
+
