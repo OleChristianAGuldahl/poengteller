76caf0adb5f68e98c337abae4dcf9a7be81d1269
OleChristianAGuldahl <olechristiangulldal@gmail.com>
2021-01-10 14:44:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
             
 
 # Lager ett tilfeldig tall mellom 0-2 
-lodd = random.randint(0, 3)
+tilfeldig_tall = random.randint(0, 3)
 
 
 
@@ -13,12 +13,12 @@ poengsum=0
                                                                    
 
 svar = ""
-
+#går gjenno loop, sjekker svar og lager poensum
 while svar != "99":
     svar = input("Gjett ett tall mellom 0 og 10 (skriv 99 for å avslutte spillet): ")
-    if int(svar) == int(lodd):
+    if int(svar) == int(tilfeldig_tall):
         poengsum +=1
         print ("Riktig, din poensum er nå: " + str(poengsum))
-        lodd = random.randint(0, 2)
+        tilfeldig_tall = random.randint(0, 2)
     else:
         print("Desverre feil, gjett igjen")
